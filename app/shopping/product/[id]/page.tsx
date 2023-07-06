79743a340ad3150b57aef7eff709a308aaadef20
Faysal Bsata <faysalbsata@hotmail.com>
2023-07-06 17:03:29 +0400

    :wrench:

---
+++ app/shopping/product/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { getFetchUrl } from '@/lib/getFetchUrl';
+import { StarIcon } from '@heroicons/react/24/solid';
+import { notFound } from 'next/navigation';
+export const revalidate = 300;
+function ProductPage() {
+  return <div>ProductPage</div>;
+}
+
+export default ProductPage;
